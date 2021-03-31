121257271faf3ff6618fda5cf968082f50e65f8c
Pi-Hsun Shih <pihsun@chromium.org>
2021-03-31 15:05:56 +0800

    WIP: cros_usbpd_notify: Also listen to EC_HOST_EVENT_USB_MUX host event.
    BUG=b:183562289
    TEST=manually
    
    Change-Id: Iaa5ac9c3d8d473e6ef41f9fbdcb190e80797c719

--- drivers/platform/chrome/cros_usbpd_notify.c
+++ drivers/platform/chrome/cros_usbpd_notify.c
@@ -220,7 +220,9 @@ static int cros_usbpd_notify_plat(struct notifier_block *nb,
 	if (!host_event)
 		return NOTIFY_DONE;
 
-	if (host_event & EC_HOST_EVENT_MASK(EC_HOST_EVENT_PD_MCU)) {
+	/* TODO(pihsun): Check what to do on ACPI system for EC_HOST_EVENT_USB_MUX */
+	if (host_event & (EC_HOST_EVENT_MASK(EC_HOST_EVENT_PD_MCU) |
+			  EC_HOST_EVENT_MASK(EC_HOST_EVENT_USB_MUX))) {
 		cros_usbpd_get_event_and_notify(pdnotify->dev, ec_dev);
 		return NOTIFY_OK;
 	}
